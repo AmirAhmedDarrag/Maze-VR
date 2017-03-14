1eb984c7901f4982aa140c5dc90b19f4f23a4546
Amir Ahmed <amirdarrag2013@gmail.com>
2017-03-14 20:27:02 +0200

    fix: first commit

---
+++ Assets/Udacity/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour 
+{
+    //Create a reference to the CoinPoofPrefab
+    public GameObject CoinPoofPrefab;
+
+    public void OnCoinClicked() {
+        // Instantiate the CoinPoof Prefab where this coin is located
+        // Make sure the poof animates vertically
+        Object.Instantiate(CoinPoofPrefab, transform.position, Quaternion.identity);
+        // Destroy this coin. Check the Unity documentation on how to use Destroy
+        Destroy(gameObject);
+    }
+}
